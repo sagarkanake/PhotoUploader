da2bad036a8bb6ddebd9593f9a9015ba985b05b0
isagar33 <9284sagarkanake@gmail.com>
2021-12-19 13:11:27 +0530

    Login Enabled in Borwsable Api of session authentication"

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('api/', include('accounts.api.urls'))
+    path('api/', include('accounts.api.urls')),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
